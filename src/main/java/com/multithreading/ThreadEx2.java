9cea698c27282e2b4baefd374dc1edddddc36f11
bootcoding#123 <ModakAkshali211@gmail.com>
2023-09-11 12:00:22 +0530

    Multithreading demo

---
+++ src/main/java/com/multithreading/ThreadEx2.java
@@ -0,0 +1,16 @@
+package com.multithreading;
+
+public class ThreadEx2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
+
+        Thread t1 = new NumberOfThread();
+        t1.start();
+        Thread t2 = new AlphaThread();
+        t2.start();
+        Thread t3 = new SpecialSymbolOfThread();
+        t3.start();
+    }
+}
